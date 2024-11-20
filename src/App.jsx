a6271dfc342d4c89d8c83a88f6d6bd936a4f82a3
datmedevil17 <rakshitshukla17@gmail.com>
2024-11-20 23:12:56 +0530

    physics code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Physics } from "@react-three/rapier";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas shadows camera={{ position: [10, 10, 10], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
+      <Suspense>
+      <Physics>
       <Experience />
+      </Physics>
+      </Suspense>
     </Canvas>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls,Box,Sphere } from "@react-three/drei";
+import { RigidBody } from "@react-three/rapier";
+import { useState,useRef } from "react";
 
 export const Experience = () => {
+    const [hover,setHover] = useState(false)
+    const cube = useRef()
+    const jump =()=>{
+        cube.current.applyImpulse({x:0,y:5,z:0})
+
+    }
   return (
     <>
+    <ambientLight intensity={0.5}/>
+    <directionalLight position={[-10,10,0]} intensity={0.4}/>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <RigidBody ref={cube} position={[-2.5,1,0]}>
+        <Box onPointerEnter={()=>setHover(true)} onPointerLeave={()=>setHover(false)}
+            onClick={jump}>
+            <meshStandardMaterial color={hover ? "hotpink" : "royalblue"}/>
+        </Box>
+      </RigidBody>
+
+      <RigidBody type="fixed">
+        <Box position={[0,0,0]} args={[10,1,10]}>
+            <meshStandardMaterial color="springgreen"/>
+        </Box>
+      </RigidBody>
     </>
   );
 };
